e468373ecd9c84a8746dd3407908d010a7b41bd8
jameseclement <jameseclement@gmail.com>
2019-01-06 15:04:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,6 @@ require 'pry'
 
 ## defines method which accepts a string called sentence1
 def reverse_each_word(sentence1)
-  binding.pry
-  
   ## sentence1.split turns the string into an array, where each word is an element of the array. No spaces.
  sentence_array = sentence1.split
  sentence_array.collect { |word| word.reverse!

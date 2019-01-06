4d04e965d3bc6b70f50b51ca4a10085d7e1bc459
jameseclement <jameseclement@gmail.com>
2019-01-06 15:02:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,8 @@ require 'pry'
 ## defines method which accepts a string called sentence1
 def reverse_each_word(sentence1)
   binding.pry
+  
+  ## sentence1.split
  sentence_array = sentence1.split
  sentence_array.collect { |word| word.reverse!
    binding.pry

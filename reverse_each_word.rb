52464167d7b3776a0de90d0ad405a7b33fccc953
jameseclement <jameseclement@gmail.com>
2019-01-06 15:00:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,8 @@
 def reverse_each_word(sentence1)
+  binding.pry
  sentence_array = sentence1.split
- sentence_array.collect { |word| word.reverse!}
+ sentence_array.collect { |word| word.reverse!
+   binding.pry
+ }
   sentence_array.join(" ")
 end
